dd46f758fd64475c47867e09800654588b3e387c
KacperOgrodniczuk <85175239+KacperOgrodniczuk@users.noreply.github.com>
2024-12-12 16:49:29 +0000

    Demo 10 - 2D AI Navigation

---
+++ Assets/Demos/Demo 10 - Enemy AI/BasicEnemy.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicEnemy : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
