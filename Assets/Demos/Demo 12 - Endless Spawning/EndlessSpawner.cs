dac1a49777220ffdc87d63e69f41f5fb0107ae0a
KacperOgrodniczuk <85175239+KacperOgrodniczuk@users.noreply.github.com>
2025-01-21 09:51:42 +0000

    Endless Spawner Demo

--- Assets/Demos/Demo 12 - Endless Spawning/EndlessSpawner.cs
+++ Assets/Demos/Demo 12 - Endless Spawning/EndlessSpawner.cs
@@ -5,10 +5,10 @@ public class EndlessSpawner : MonoBehaviour
 {
     public GameObject objectToSpawn; // The prefab to spawn
     public Transform spawnPoint;     // The point where objects will be spawned
-    
-    float spawnInterval = 2f; // Time between spawns in seconds
-    float minSpawnInterval = 1f;
-    float intervalDecrease = 0.1f;
+
+    float spawnInterval = 2f;       // Time between spawns in seconds
+    float minimumSpawnInterval = 1f;    // The minimal amount of time between enemies spawning.
+    float intervalDecrease = 0.1f;  // How much does the spawn time decrease by.
 
     private void Start()
     {
@@ -29,7 +29,11 @@ public class EndlessSpawner : MonoBehaviour
                 Debug.LogWarning("Object to spawn or spawn point is not set.");
             }
 
+            // Wait between spawn times
             yield return new WaitForSeconds(spawnInterval);
+
+            //calculate the new spawn time.
+            spawnInterval = Mathf.Max(minimumSpawnInterval, spawnInterval - intervalDecrease);
         }
     }
 }
